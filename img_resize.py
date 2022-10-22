ee40a35805707d127d126ea9de3cf9433507409c
s40523122 <40523122@gm.nfu.edu.tw>
2022-10-22 13:53:45 +0800

    add data

---
+++ img_resize.py
@@ -0,0 +1,20 @@
+import cv2
+import glob
+
+for img in glob.glob("raw/*.jpg"):
+    im = cv2.imread(img)
+    #im = im[:3840, :2160]
+    #resize_im = cv2.resize(im, (720, 1280), interpolation=cv2.INTER_AREA)
+    resize_im = cv2.resize(im, (734, 979), interpolation=cv2.INTER_AREA)
+    resize_im = resize_im[:978, :]
+    resize_im = cv2.resize(im, (367, 489), interpolation=cv2.INTER_AREA)
+    cv2.imshow('Result', resize_im)
+    cv2.waitKey(0)
+    cv2.imwrite("resize"+img[3:], resize_im)
+    #break
+'''
+image = cv2.imread('lena.jpg')
+image = cv2.resize(image, (256, 256), interpolation=cv2.INTER_AREA)
+cv2.imshow('Result', image)
+cv2.waitKey(0)
+'''
