372f0dcd79895960003500c6817bc1955e166d82
s40523122 <40523122@gm.nfu.edu.tw>
2022-11-14 15:17:50 +0800

    add 1280

--- img_resize.py
+++ img_resize.py
@@ -1,16 +1,16 @@
 import cv2
 import glob
 
-for img in glob.glob("raw/*.jpg"):
+for img in glob.glob("square/*.jpg"):
     im = cv2.imread(img)
     #im = im[:3840, :2160]
     #resize_im = cv2.resize(im, (720, 1280), interpolation=cv2.INTER_AREA)
-    resize_im = cv2.resize(im, (734, 979), interpolation=cv2.INTER_AREA)
-    resize_im = resize_im[:978, :]
-    resize_im = cv2.resize(im, (367, 489), interpolation=cv2.INTER_AREA)
+    #resize_im = cv2.resize(im, (734, 979), interpolation=cv2.INTER_AREA)
+    #resize_im = resize_im[:978, :]
+    resize_im = cv2.resize(im, (1280, 1280), interpolation=cv2.INTER_AREA)
     cv2.imshow('Result', resize_im)
     cv2.waitKey(0)
-    cv2.imwrite("resize"+img[3:], resize_im)
+    cv2.imwrite("square_1280"+img[6:], resize_im)
     #break
 '''
 image = cv2.imread('lena.jpg')
